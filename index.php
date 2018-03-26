f977f61bd0e9059348de89308ace768d9dca5d76
satoken912 <seat.soil.74196901@gmail.com>
2018-03-26 15:55:44 +0900

    First commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <script src="../main.js"></script>
+    <title>自己紹介</title>
+</head>
+<body>
+    <div class="nav fixed">
+        <ul id="menubar">
+            <li><a href="http://satoken.nkmr.io/">HOME</a></li>
+            <li><a href="http://satoken.nkmr.io/profile/">PROFILE</a></li>
+            <li><a href="http://satoken.nkmr.io/research/">RESEARCH</a></li>
+            <li><a href="http://satoken.nkmr.io/product/">PRODUCT</a></li>
+            <li><a href="http://satoken.nkmr.io/interest/">INTEREST</a></li>
+            <li><a href="http://satoken.nkmr.io/activity/">ACTIVITY</a></li>
+            <li><a href="http://satoken.nkmr.io/link/">LINK</a></li>
+        </ul>
+    </div>
+    <img id="header" src="http://satoken.nkmr.io/img/header.jpg">
+
+    <div id="content">
+        <div class="heading">自己紹介</div>
+        <table>
+            <tr><td><img class="symbol" src="../img/profile/name.png"></td><td>佐藤 剣太（さとう けんた）</td></tr>
+            <tr><td><img class="symbol" src="../img/profile/birthday.png"></td><td>1994.06.17</td></tr>
+            <tr><td><img class="symbol" src="../img/profile/hometown.png"></td><td>埼玉県 深谷市</td></tr>
+            <tr><td><img class="symbol" src="../img/profile/school.png"></td><td>明治大学 先端数理科学研究科 先端メディアサイエンス専攻 1年<br>中村聡史研究室</td></tr>
+            <tr><td><img class="symbol" src="../img/profile/research.png"></td><td>ヒューマンコンピュータインタラクション，手書き文字，コミック工学</td></tr>
+        </table>
+    </div>
+</body>
+</html>
